ebf23cc866b01d63fe34a7676319fdbc6e51da66
frehn <fraini@gmail.com>
2015-02-04 19:39:07 +0100

    Derive clauses by factoring in fixDerivation.
    fixDerivation checks if a clause can be derived by factoring by using
    subsumption.
    A unit test for this feature is added as well.

--- src/main/scala/at/logic/algorithms/resolution/fixDerivation.scala
+++ src/main/scala/at/logic/algorithms/resolution/fixDerivation.scala
@@ -8,6 +8,7 @@ import at.logic.language.fol.{Equation => FOLEquation, FOLTerm, FOLFormula, FOLE
 import at.logic.calculi.resolution.{FClause, Clause}
 import at.logic.algorithms.lk.{applySubstitution => applySub, CleanStructuralRules, CloneLKProof}
 import at.logic.provers.atp.SearchDerivation
+import at.logic.algorithms.subsumption.StillmanSubsumptionAlgorithmFOL
 
 /**
 *  Sometimes, we have a resolution refutation R of a set of clauses C
@@ -16,7 +17,8 @@ import at.logic.provers.atp.SearchDerivation
 *  This algorithm tries to obtain R' by trying to replace clauses c
 *  from C in R by derivations of C from C' in the following way:
 *
-*  - If c is in C', do nothing.
+*  - If c is in C' or c is an instance of reflexivity, do nothing.
+*  - If c is subsumed by some c' in C', derive c from c' by factoring.
 *  - Otherwise, try to derive c from C' by paramodulation and symmetry (prover9 often needs
 *    this, and the check is usually fast),
 *  - Otherwise, try to derive c from C' by propositional resolution.
@@ -87,6 +89,8 @@ object fixDerivation extends at.logic.utils.logging.Logger {
   }
 
   private def deriveBySymmetry( to: FClause, from: FSequent ) = {
+    trace("deriving " + to + " from " + from + " by symmetry")
+
     val (neg_map, pos_map) = getSymmetryMap( to, from ).get
 
     val init = InitialClause(from.antecedent.map(_.asInstanceOf[FOLFormula]), from.succedent.map(_.asInstanceOf[FOLFormula]))
@@ -101,34 +105,76 @@ object fixDerivation extends at.logic.utils.logging.Logger {
     })
   }
 
+  private val subsumption_alg = StillmanSubsumptionAlgorithmFOL
+
+  def canDeriveByFactor( to: FClause, from: FSequent ) = 
+    subsumption_alg.subsumes( from, to.toFSequent )
+
+  def deriveByFactor( to: FClause, from: FSequent ) : RobinsonResolutionProof =
+  {    
+    trace("deriving " + to + " from " + from + " by factoring")
+    val init = InitialClause(from.antecedent.map(_.asInstanceOf[FOLFormula]), from.succedent.map(_.asInstanceOf[FOLFormula]))
+    deriveByFactor( to, init )
+   }
+
+  def deriveByFactor( to: FClause, from: RobinsonResolutionProof ) : RobinsonResolutionProof =
+  {
+    val from_c = FSequent( from.root.antecedent.map( _.formula ), from.root.succedent.map( _.formula ) )
+    val s = subsumption_alg.subsumes_by( from_c, to.toFSequent ).get
+    val from_s = FClause( from_c.antecedent.map( s(_) ), from_c.succedent.map( s(_) ) )
+    // make a first Factor inference that does not contract, but applies
+    // the substitution
+    val first = if (!from_c.antecedent.isEmpty)
+      Factor(from, from_c.antecedent.head, 1, false, s)
+    else
+      Factor(from, from_c.succedent.head, 1, true, s)
+    val proof = from_s.neg.toSet.foldLeft( first )( (proof, atom) => {
+      val cnt = from_s.neg.count( _ == atom ) - to.neg.count( _ == atom ) + 1
+      Factor(proof, atom, cnt, false, Substitution())
+    } )
+    from_s.pos.toSet.foldLeft( proof )( (proof, atom) => {
+      val cnt = from_s.pos.count( _ == atom ) - to.pos.count( _ == atom ) + 1
+      Factor(proof, atom, cnt, true, Substitution())     
+    } ) 
+
+  }
+
+  private def isReflexivity(c: FClause) = 
+    c.pos.exists( a => a match {
+        case FOLEquation(x, y) if x == y => true
+        case _ => false
+      } )
+
+  private def isTautology(c: FClause) = c.pos.exists( a => c.neg.exists( b => a == b ) )
+
   // NOTE: What if the symmetric clause found is a tautology?
   private def handleInitialClause( cls: FClause, cs: Seq[FSequent] ) = {
-      val cls_sequent = FSequent(
+    val cls_sequent = FSequent(
       cls.neg.map (f => f.asInstanceOf[FOLFormula]), 
       cls.pos.map (f => f.asInstanceOf[FOLFormula]))
 
-    if (cs.contains(cls_sequent)) InitialClause(cls)
+    if ( cs.contains(cls_sequent) || isReflexivity(cls) || isTautology(cls) ) InitialClause(cls)
     else
       cs.find( c => canDeriveBySymmetry( cls, c ) ) match {
         case Some( c ) => deriveBySymmetry( cls, c )
-        case None => SearchDerivation(cs, cls_sequent, true) match {
-          case Some(d) => {
-            val ret = d.asInstanceOf[RobinsonResolutionProof]
-            if (ret.root.toFClause != cls)
-            {
-              // TODO: add appropriate rules to derive cls --- SearchDerivation only yields
-              // a derivation of a clause subsuming cls, not cls itself!
-              // Until this is fixed, we just return the original clause, not the derivation
-              // constructed above.
-
-              // trace("WRONG ENDCLAUSE: " + ret.root  + " instead of " + cls)
-              InitialClause(cls) 
-            } else
-              ret
-          }
-          case None => {
-            warn("Could not derive " + cls + " from " + cs + " by symmetry or propositional resolution")
-            InitialClause(cls)
+        case None => cs.find( c => canDeriveByFactor( cls, c ) ) match {
+          case Some( c ) => deriveByFactor( cls, c )
+          case None => SearchDerivation(cs, cls_sequent, true) match {
+            case Some(d) => {
+              val ret = d.asInstanceOf[RobinsonResolutionProof]
+              if (ret.root.toFClause != cls)
+              {
+                if (canDeriveByFactor( cls, FSequent( ret.root.antecedent.map(_.formula), ret.root.succedent.map(_.formula))))
+                  deriveByFactor( cls, ret )
+                else
+                  InitialClause(cls) 
+              } else
+                ret
+            }
+            case None => {
+              warn("Could not derive " + cls + " from " + cs + " by symmetry or propositional resolution")
+              InitialClause(cls)
+            }
           }
         }
       }
--- src/test/scala/at/logic/algorithms/resolution/fixDerivationTest.scala
+++ src/test/scala/at/logic/algorithms/resolution/fixDerivationTest.scala
@@ -4,12 +4,12 @@ import org.junit.runner.RunWith
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.runner.JUnitRunner
 import at.logic.language.lambda.types.To
-import at.logic.language.fol.{Equation => FOLEquation, And, Or, Neg, Atom, FOLConst, Imp}
+import at.logic.language.fol.{Equation => FOLEquation, And, Or, Neg, Atom, FOLConst, Imp, FOLVar}
 import at.logic.calculi.resolution.FClause
 import at.logic.calculi.lk.base.FSequent
 
 @RunWith(classOf[JUnitRunner])
-class SymmetryTest extends SpecificationWithJUnit {
+class fixDerivationTest extends SpecificationWithJUnit {
   "fixDerivation" should {
     "not say that p :- is derivable from p :- p, r by symmetry" in {
       val p = Atom( "p", Nil )
@@ -34,6 +34,25 @@ class SymmetryTest extends SpecificationWithJUnit {
       val to = FClause( cb::ab::Nil, dc::Nil )
 
       fixDerivation.canDeriveBySymmetry( to, from ) must beTrue
+    }
+
+    "say that p(a) :- q(x) can be derived by factoring from p(x), p(y) :- q(u), q(v)" in {
+      val a = FOLConst("a")
+      val x = FOLVar("x")
+      val y = FOLVar("y")
+      val u = FOLVar("u")
+      val v = FOLVar("v")
+      val pa = Atom("p", a::Nil)
+      val px = Atom("p", x::Nil)
+      val py = Atom("p", y::Nil)
+      val qx = Atom("q", x::Nil)
+      val qu = Atom("q", u::Nil)
+      val qv = Atom("q", v::Nil)
+
+      val to = FClause( pa::Nil, qx::Nil )
+      val from = FSequent( px::py::Nil, qu::qv::Nil )
+
+      fixDerivation.canDeriveByFactor( to, from ) must beTrue
     }
   }
 }
